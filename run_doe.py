e3f68f03281741d8738d373d8cb68927fe170897
Jehan <jehan.dastoor@mail.mcgill.ca>
2022-03-20 00:15:13 -0400

    bug in .gitignore

--- run_doe.py
+++ run_doe.py
@@ -9,7 +9,6 @@ from itertools import repeat
 from datetime import datetime as dt
 import pykep as pk
 import pickle
-from spice_interpolation import new_planet, new_planet2
 
 """
 Two main functions:
@@ -142,7 +141,7 @@ if __name__ == "__main__":
     
     start = dt.now()
     input_filename = "test.csv"
-    output_filename = "../results/AEON_" + dt.date(start).isoformat() + ".csv"
+    output_filename = "results/AEON_" + dt.date(start).isoformat() + ".csv"
     planet_dic = {1:earth, 2:venus, 3:mars, 4:jupiter, 5:None}
     departure_window = [] #### NEED TO FIX THIS EVENTUALLY
     target = titan
